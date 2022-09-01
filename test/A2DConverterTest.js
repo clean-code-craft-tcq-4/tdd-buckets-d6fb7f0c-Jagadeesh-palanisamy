c6cca953f2a055af58c99a005246eba8cec05c06
Jagadeesh Palanisamy <palanisamy.jagadeesh@in.bosch.com>
2022-09-01 10:20:07 +0530

    changes in a2d test file

--- test/A2DConverterTest.js
+++ test/A2DConverterTest.js
@@ -6,7 +6,7 @@ describe('integration test', () => {
   it('should convert 12 bit values to ranges', () => {
     const inputs = [0, 1024, 256, 3500, 2000, 1500, 4094];
     expect(
-        rangeCounter.getRangeCounter(
+        rangeCounter.getRange(
             a2dConverter.convertArrayA2D(inputs, 10, 4094),
         ),
     ).to.deep.equal({'0-1': 2, '3-5': 3, '9-10': 2});
@@ -14,7 +14,7 @@ describe('integration test', () => {
   it('should throw error for invalid input', () => {
     const inputs = [0, 1024, 256, 3500, 2000, 1500, 4096];
     expect(() => {
-      rangeCounter.getRangeCounter(
+      rangeCounter.getRange(
           a2dConverter.convertArrayA2D(inputs, 10, 4094),
       );
     }).to.throw('invalid input');
